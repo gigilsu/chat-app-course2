b23effef798113b895e92bbe22a03b23004a67da
gigilsu <gigi.lsu@gmail.com>
2018-01-08 14:52:30 -0600

    add stylesheet for chat page

--- server/utils/message.js
+++ server/utils/message.js
@@ -5,13 +5,25 @@ var generateMessage = (from, text) => {
     createdAt: new Date().getTime()
   };
 };
-
+// key=AIzaSyDzcG0Qo51H3j24RsAOeb3_50Gc2QWUVvI
 var generateLocationMessage = (from, latitude, longitude) => {
   return {
     from,
     url: `https://www.google.com/maps?q=${latitude},${longitude}`,
     createdAt: new Date().getTime()
   };
+}
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
 
 module.exports = {generateMessage, generateLocationMessage};
